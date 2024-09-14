9663d76a796e68d654e78ed4e11ffbe012246c37
hiromuota166 <otahiromu166@gmail.com>
2024-09-14 22:04:14 +0900

    add:ログイン機能を実装。名前と写真URLを取得し、usersテーブルに挿入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,41 @@
-'use client';
-import { useEffect } from "react";
-import { requestPermission } from "../messaging_init_in_sw";
+"use client"
+import { useAuth } from "@/context/auth";
+import { login, logout } from "@/lib/auth";
+import { useState } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    console.log("呼んだ？");
-    requestPermission();
-  }, []);
+  const user = useAuth();
+  const [waiting, setWaiting] = useState<boolean>(false);
+
+  const signIn = () => {
+    setWaiting(true);
+
+    login()
+      .catch((error) => {
+        console.error(error?.code);
+      })
+      .finally(() => {
+        setWaiting(false);
+      });
+  };
   return (
     <div>
-      <h1>こんにちは!!!神宮111</h1>
+      {user === null && !waiting && (
+        <button
+          onClick={signIn}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+        >
+          ログイン
+        </button>
+      )}
+      {user && (
+        <button
+          onClick={logout}
+          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition-colors"
+        >
+          ログアウト
+        </button>
+      )}
     </div>
   );
 }
